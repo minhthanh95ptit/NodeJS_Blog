0ad47da2fd41a2edc054cddc4864812469a83a4e
THANHPM <minhthanh95ptit@gmail.com>
2020-06-04 17:37:53 +0700

    Blog chapter 4 - templating engine - ejs

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = new express();
 
+const ejs = require('ejs');
+app.set('view engine', 'ejs');
+
+
 app.use(express.static('public'));
 
 app.listen(4000, () => {
@@ -9,6 +13,18 @@ app.listen(4000, () => {
 })
 
 
-app.get('/', (request, response) => {
-    response.sendFile(path.resolve(__dirname, 'pages/index.html'))
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
+app.get('/about', (req, res) => {
+    res.render('about');
+})
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+})
+
+app.get('/post', (req, res) => {
+    res.render('post');
 })
