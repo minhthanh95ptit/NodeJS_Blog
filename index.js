c1b8e94bba582bca254a8093c3ecc8f616ea65fa
THANHPM <minhthanh95ptit@gmail.com>
2020-06-05 12:04:57 +0700

    Chapter 7

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const app = new express();
 const ejs = require('ejs');
 app.set('view engine', 'ejs');
 
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/my_database', { useNewUrlParser: true })
+
+const BlogPost = require('./models/BlogPost.js');
+
 
 app.use(express.static('public'));
 
@@ -14,7 +23,12 @@ app.listen(4000, () => {
 
 
 app.get('/', (req, res) => {
-    res.render('index');
+    BlogPost.find({}, function (error, posts) {
+        console.log(posts);
+        res.render('index', {
+            blogposts: posts
+        })
+    });
 })
 
 app.get('/about', (req, res) => {
@@ -25,6 +39,22 @@ app.get('/contact', (req, res) => {
     res.render('contact');
 })
 
-app.get('/post', (req, res) => {
-    res.render('post');
+app.get('/post/:id', (req, res) => {
+    BlogPost.findById(req.params.id, function (error, detailPost) {
+        res.render('post', {
+            detailPost
+        })
+    })
+})
+
+app.get('/posts/new', (req, res) => {
+    res.render('create');
+})
+
+app.post('/posts/store', (req, res) => {
+    console.log(req.body);
+
+    BlogPost.create(req.body, (error, blogpost) => {
+        res.redirect('/');
+    })
 })
