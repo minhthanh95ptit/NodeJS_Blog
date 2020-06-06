63fa5b03320871b8d6b661adb10840b312f54082
THANHPM <minhthanh95ptit@gmail.com>
2020-06-06 16:47:31 +0700

    Chapter 11-Express-Session

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const ejs = require('ejs');
 
+
+
 const app = new express();
+
+const expressSession = require('express-session');
+
+
 const newPostController = require('./controllers/newPost')
 const homeController = require('./controllers/home.js')
 const getPostController = require('./controllers/getPost')
@@ -12,9 +18,11 @@ const registerController = require('./controllers/newUser')
 const storeUserController = require('./controllers/storeUser')
 const loginUserController = require('./controllers/loginUser')
 const loginController = require('./controllers/login')
-
+const logoutController = require('./controllers/logout')
 
 const validateMiddleWare = require('./middlewares/validationMiddleware')
+const authMiddleWare = require('./middlewares/authMiddleware')
+const redirectIfAuthenticatedMiddleware = require('./middlewares/redirectIfAuthenticatedMiddleware')
 
 app.set('view engine', 'ejs');
 
@@ -32,10 +40,22 @@ mongoose.connect('mongodb://localhost/my_database', { useNewUrlParser: true })
 
 
 
+app.use(expressSession({
+    secret: 'keyboard cat'
+}))
+
+
 app.use(express.static('public'));
 
 app.listen(4000, () => {
     console.log('App listening on port 4000')
+})
+
+global.loggedIn = null;
+
+app.use("*", (req, res, next) => {
+    loggedIn = req.session.userId;
+    next()
 })
 
 app.get('/', homeController);
@@ -46,13 +66,19 @@ app.get('/contact', contactControler)
 
 app.get('/post/:id', getPostController)
 
-app.get('/posts/new', newPostController)
+app.get('/posts/new', authMiddleWare, newPostController)
 
-app.post('/posts/store', validateMiddleWare, storePostController)
+app.post('/posts/store', authMiddleWare, validateMiddleWare, storePostController)
 
-app.get('/auth/register', registerController)
+app.get('/auth/register', redirectIfAuthenticatedMiddleware, registerController)
 
-app.post('/users/register', storeUserController)
+app.post('/users/register', redirectIfAuthenticatedMiddleware, storeUserController)
 
-app.get('/auth/login', loginController)
-app.post('/users/login', loginUserController)   
+app.get('/auth/login', redirectIfAuthenticatedMiddleware, loginController)
+app.post('/users/login', redirectIfAuthenticatedMiddleware, loginUserController)
+
+app.get('/auth/logout', logoutController)
+
+app.use((req, res) => {
+    res.render('404')
+})
